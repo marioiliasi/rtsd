bf0a325be68885e571b8e100be19d1632e2b6b76
marioiliasi <marioiliasi@waterford.org>
2020-03-16 18:51:11 +0200

    first commit - added elasticsearch & voltDB start & stop scripts

---
+++ volt/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker pull voltdb/voltdb-community
+
+docker stop volt-test-master || true
+docker rm volt-test-master || true
+
+#docker network create -d bridge voltLocalCluster
+
+docker run -d -P \
+	-e HOST_COUNT=1 -e HOSTS=volt-test-master \
+	--name volt-test-master  \
+	-p 8080:8080 -p 21212:21212 -p 21211:21211 \
+	--network=voltLocalCluster voltdb/voltdb-community:latest
---
+++ volt/stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker stop volt-test-master
+docker rm volt-test-master
